bd39c479001dc1f42a3a22c2172c10c9db2a3b61
Imtiaz Mahmood <imtiazmehmood26@gmail.com>
2023-04-09 17:29:37 +0500

    feat: Nested navigation & map directions

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -2,14 +2,30 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
 import MapsView from '../components/MapsView';
+import {createStackNavigator} from '@react-navigation/stack';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionCard from '../components/RideOptionCard';
 const MapScreen = () => {
+  const CardStack = createStackNavigator();
   return (
     <View>
-      <Text>MapScreen</Text>
       <View style={tw`h-1/2`}>
         <MapsView />
       </View>
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <CardStack.Navigator screenOptions={{headerShown: false}}>
+          <CardStack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{headerShown: false}}
+          />
+          <CardStack.Screen
+            name="RideOptionCard"
+            component={RideOptionCard}
+            options={{headerShown: false}}
+          />
+        </CardStack.Navigator>
+      </View>
     </View>
   );
 };
